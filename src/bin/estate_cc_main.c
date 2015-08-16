f56762ee7f5367318f320b802dbce4c4bacb2b64
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2015-08-16 21:48:33 +0200

    estate_cc: add decent getopt

--- src/bin/estate_cc_main.c
+++ src/bin/estate_cc_main.c
@@ -1,15 +1,73 @@
 #include "estate_cc.h"
+#include <getopt.h>
 
 int _estate_cc_dom = -1;
+
+static const struct option _options[] =
+{
+     {"output",  required_argument, NULL, 'o'},
+     {"verbose", no_argument,       NULL, 'v'},
+     {"help",    no_argument,       NULL, 'h'},
+     {NULL,      0,                 NULL,  0}
+};
+
+static void
+_usage(char const *const prog,
+       FILE       *      stream)
+{
+   fprintf(stream,
+           "Usage: %s [OPTIONS] file\n"
+           "\n"
+           "With the following options:\n"
+           "  -o|--output    specify the output file. stdout by default\n"
+           "  -v|--verbose   cumulative, increase the verbosity level\n"
+           "  -h|--help      display this message\n"
+           "\n",
+           prog);
+}
 
 int
 main(int    argc,
      char **argv)
 {
    int ret = EXIT_FAILURE;
-   char const *file;
+   char const *input;
+   char const *output = NULL;
    Parser *p;
    Eina_List *parse;
+   int c;
+   unsigned int verbosity = 0;
+
+   /* Getopt */
+   while (1)
+     {
+        c = getopt_long(argc, argv, "o:vh", _options, NULL);
+        if (c == -1) break;
+        switch (c)
+          {
+           case 'o':
+              output = optarg;
+              break;
+
+           case 'v':
+              verbosity++;
+              break;
+
+           case 'h':
+              _usage(argv[0], stdout);
+              return EXIT_SUCCESS;
+
+           default:
+              _usage(argv[0], stderr);
+              goto end;
+          }
+     }
+   if (argc - optind != 1)
+     {
+        _usage(argv[0], stderr);
+        goto end;
+     }
+   input = argv[optind];
 
    /* Init Eina */
    if (EINA_UNLIKELY(!eina_init()))
@@ -25,13 +83,6 @@ main(int    argc,
         goto eina_shut;
      }
 
-   if (argc != 2)
-     {
-        ERR("Usage: %s file.est", argv[0]);
-        goto log_shut;
-     }
-   file = argv[1];
-
    /* Setup parser */
    p = estate_cc_parser_new();
    if (EINA_UNLIKELY(!p))
@@ -39,17 +90,23 @@ main(int    argc,
         CRI("Failed to create parser");
         goto log_shut;
      }
-   if (EINA_UNLIKELY(!estate_cc_parser_file_set(p, file)))
+   if (EINA_UNLIKELY(!estate_cc_parser_file_set(p, input)))
      {
         ERR("Failed to set parser");
         goto parser_free;
      }
 
+   /* Parse */
    parse = estate_cc_parser_parse(p);
-   estate_cc_parser_parse_free(p);
+   if (!parse)
+     {
+        ERR("Parsing of file \"%s\" failed", input);
+        goto parser_free;
+     }
+
 
    ret = EXIT_SUCCESS;
-
+   estate_cc_parser_parse_free(p);
 parser_free:
    estate_cc_parser_free(p);
 log_shut:
