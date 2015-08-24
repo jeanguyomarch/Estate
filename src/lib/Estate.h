be7c57da2e285dab9a4bc2560071628f97b1f0e6
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2015-08-24 23:47:47 +0200

    doc: add Doxygen

--- src/lib/Estate.h
+++ src/lib/Estate.h
@@ -13,31 +13,65 @@
 #include <Eina.h>
 
 
-
-typedef struct _Estate_Machine Estate_Machine;
-typedef struct _Estate_State Estate_State;
-typedef struct _Estate_Transition Estate_Transition;
-
 /**
  * @defgroup Estate_Main Top level functions
  * API that affect Estate as a whole
  * @{
  */
 
+/**
+ * @typedef Estate_Machine
+ * An opaque handler for a finite state machine
+ */
+typedef struct _Estate_Machine Estate_Machine;
+
+/**
+ * @typedef Estate_State
+ * An opaque handler for a state
+ */
+typedef struct _Estate_State Estate_State;
+
+/**
+ * @typedef Estate_Transition
+ * An opaque handler for a transition
+ */
+typedef struct _Estate_Transition Estate_Transition;
+
+
+/**
+ * @enum Estate_Cb_Type
+ * Type of an Estate callback
+ */
 typedef enum
 {
-   ESTATE_CB_TYPE_ENTERER         = 0,
-   ESTATE_CB_TYPE_EXITER          = 1,
-   ESTATE_CB_TYPE_TRANSITION      = 2
+   ESTATE_CB_TYPE_ENTERER         = 0, /**< When entering a state */
+   ESTATE_CB_TYPE_EXITER          = 1, /**< When exiting a state */
+   ESTATE_CB_TYPE_TRANSITION      = 2  /**< When a transition is executing */
 } Estate_Cb_Type;
 
+/**
+ * @typedef Estate_Cb
+ * Callback raised when a transition is activated, and therefore
+ * the current state is changing
+ *
+ * @param data User data. Provided by estate_machine_data_set()
+ * @param type Which callback has been triggered
+ * @param transition The transition related to the callback
+ */
 typedef void (*Estate_Cb)(void                    *data,
                           Estate_Cb_Type           type,
                           const Estate_Transition *transition);
 
-
-
+/**
+ * Initialises the Estate library
+ * @return How many times the library has been initialized.
+ */
 EAPI int estate_init(void);
+
+/**
+ * Shuts the Estate library down
+ * @return How many times the library has been initialized.
+ */
 EAPI int estate_shutdown(void);
 
 /**
@@ -52,9 +86,37 @@ EAPI int estate_shutdown(void);
  * @{
  */
 
+/**
+ * Allocates a state in a given finite state machine
+ *
+ * @param mach The parent finite state machine
+ * @return The allocated state. NULL on failure
+ */
 EAPI Estate_State *estate_state_new(Estate_Machine *mach);
+
+/**
+ * Releases a state allocated by estate_state_new()
+ *
+ * @param st The state to be released. Shall not be NULL.
+ */
 EAPI void estate_state_free(Estate_State *st);
 
+/**
+ * Sets a pre-allocated state up
+ *
+ * @param st The pre-allocated state to set up
+ * @param name The name to attach to the state
+ * @param transitions An array of the @c transit_count transitions outgoing
+ *        from the state @c st
+ * @param transit_count The count of transitions in the @c transitions array
+ * @param enterer The function to be called when a transition ends to the state @c st
+ * @param enterer_datakey The string used to retrieve the user data upon the first 
+ *        call to @c enterer
+ * @param exiter The function to be called when a transition leaves from the state @c st
+ * @param exiter_datakey The string used to retrieve the user data upon the first
+ *        call to @c exiter
+ * @return EINA_TRUE on success, EINA_FALSE on failure
+ */
 EAPI Eina_Bool
 estate_state_init(Estate_State             *st,
                   const char               *name,
@@ -65,8 +127,19 @@ estate_state_init(Estate_State             *st,
                   Estate_Cb                 exiter,
                   const char               *exiter_datakey);
 
+/**
+ * Releases the internals of a state
+ *
+ * @param st The state to release
+ */
 EAPI void estate_state_deinit(Estate_State *st);
 
+/**
+ * Returns the name of the provided state
+ *
+ * @param st The state which name must be retrieved
+ * @return The name of the state. NULL on failure.
+ */
 EAPI const char *estate_state_name_get(const Estate_State *st);
 
 /**
@@ -80,9 +153,33 @@ EAPI const char *estate_state_name_get(const Estate_State *st);
  * @{
  */
 
+/**
+ * Allocates a transition in a given finite state machine
+ *
+ * @param mach The parent finite state machine
+ * @return The allocated state. NULL on failure.
+ */
 EAPI Estate_Transition *estate_transition_new(Estate_Machine *mach);
+
+/**
+ * Releases a state allocated bu estate_transition_new()
+ *
+ * @param tr The transition to be released. Shall not be NULL
+ */
 EAPI void estate_transition_free(Estate_Transition *tr);
 
+/**
+ * Sets a pre-allocated transition up
+ *
+ * @param tr The pre-allocated transition to set up
+ * @param name The name to attach to the transition
+ * @param from The state from which @c tr starts
+ * @param to The state from which @c tr ends
+ * @param func The function called when the transition is activated
+ * @param datakey The string used to retrieve the user data upon the first
+ *        call to @c func
+ * @return EINA_TRUE on success, EINA_FALSE on failure
+ */
 EAPI Eina_Bool
 estate_transition_init(Estate_Transition  *tr,
                        const char         *name,
@@ -91,9 +188,20 @@ estate_transition_init(Estate_Transition  *tr,
                        Estate_Cb           func,
                        const char         *datakey);
 
+/**
+ * Releases the internals of a transition
+ *
+ * @param tr The transition to release
+ */
 EAPI void estate_transition_deinit(Estate_Transition *tr);
 
-EAPI const char *estate_transition_name_get(const Estate_Transition *st);
+/**
+ * Returns the name of the provided transition
+ *
+ * @param tr The transition which name must be retrieved
+ * @return The name of the transition. NULL on failure.
+ */
+EAPI const char *estate_transition_name_get(const Estate_Transition *tr);
 
 /**
  * @}
@@ -106,30 +214,106 @@ EAPI const char *estate_transition_name_get(const Estate_Transition *st);
  * @{
  */
 
+/**
+ * Creates a finite state machine (FSM) handler.
+ * When this function returns, the FSM is considered UNLOCKED:
+ * it is possible to edit it (i.e. construct) until
+ * estate_machine_lock() is called.
+ *
+ * @param states The count of states in the finite state machine
+ * @param transitions The count of transitions in the finite state machine
+ * @return The finite state machine handler. NULL on failure
+ */
 EAPI Estate_Machine *
 estate_machine_new(unsigned int states,
                    unsigned int transitions);
 
-EAPI void
-estate_machine_free(Estate_Machine *mach);
+/**
+ * Releases a finite state machine handler
+ *
+ * @param mach The finite state machine handler to be released
+ *
+ * @see estate_machine_new()
+ */
+EAPI void estate_machine_free(Estate_Machine *mach);
 
+/**
+ * Associates a key to a value to be retrieved when a callback is called.
+ * Fails if the FSM is locked.
+ *
+ * @param mach The FSM handler
+ * @param key The key used to store and retrieve persistent user data @c value
+ * @param value The value retrieved thanks to the key @c key
+ * @return EINA_TRUE on success, EINA_FALSE on failure
+ *
+ * @see estate_machine_lock()
+ */
 EAPI Eina_Bool
 estate_machine_data_set(Estate_Machine *mach,
                         const char     *key,
                         const void     *value);
 
+/**
+ * Registers a new state in the finite state machine.
+ * Fails if the FSM is locked.
+ *
+ * @param mach The FSM handler
+ * @param state The state to register
+ * @return EINA_TRUE on success, EINA_FALSE on failure
+ *
+ * @see estate_machine_lock()
+ */
 EAPI Eina_Bool
 estate_machine_state_add(Estate_Machine     *mach,
                          const Estate_State *state);
 
+/**
+ * Registers a new transition in the finite state machine
+ * Fails if the FSM is locked.
+ *
+ * @param mach The FSM handler
+ * @param tr The transition to register
+ * @return EINA_TRUE on success, EINA_FALSE on failure
+ * @see estate_machine_lock()
+ */
 EAPI Eina_Bool
 estate_machine_transition_add(Estate_Machine          *mach,
                               const Estate_Transition *tr);
 
+/**
+ * Gets the current state in the finite state machine
+ * Fails if the FSM is NOT locked.
+ *
+ * @param mach The FSM handler
+ * @return The current state in the finite state machine
+ * @see estate_machine_lock()
+ */
 EAPI Estate_State *estate_machine_current_state_get(const Estate_Machine *mach);
 
+/**
+ * Locks the finite state machine handler. After this function returns, the FSM
+ * cannot be edited (transitions and states cannot be added, data keys cannot
+ * be modified) and estate_machine_current_state_get() becomes valid and
+ * will return @c current until a transition occurs.
+ *
+ * @param mach The FSM handler
+ * @param current The init state: starting point of the FSM
+ */
 EAPI void estate_machine_lock(Estate_Machine *mach, const Estate_State *current);
 
+/**
+ * If the current state can make the transition @c transition, then the FSM
+ * does the transition:
+ *   - calls the exiter callback (if any) from the current state
+ *   - calls the transition callback (if any) for the current transition
+ *   - calls the enterer callback (if any) (from the next state)
+ * Fails if the FSM is NOT locked.
+ *
+ * @param mach The FSM handler
+ * @param transition The transition that the current state should do
+ * @return EINA_TRUE on success, EINA_FALSE on failure
+ * @see estate_machine_lock()
+ */
 EAPI Eina_Bool
 estate_machine_transition_do(Estate_Machine *mach,
                              const char     *transition);
