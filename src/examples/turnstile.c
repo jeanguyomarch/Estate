704a8c889c115be9451d88a70cbaa0ac478885c3
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2015-08-24 22:00:50 +0200

    example/turnstile: usecase

--- src/examples/turnstile.c
+++ src/examples/turnstile.c
@@ -1,47 +1,86 @@
 #include <Estate.h>
+#include "turnstile.est.c"
 
-ESTATE_MACHINE_DECLARE(turnstile);
+/* Use the prototype below to export the loading function */
+Estate_Machine *estate_fsm_turnstile_load(void) EINA_MALLOC;
 
-Eina_Bool estate_fsm_turnstile_load(void);
-
-Eina_Bool
+Estate_Machine *
 estate_fsm_turnstile_load(void)
 {
-   const Eina_Bool ok = _estate_fsm_turnstile_load();
-   return ok;
+   return _estate_fsm_turnstile_load();
 }
 
 static void
-_push(void                    *data,
-      Estate_Cb_Type           type,
-      const Estate_Transition *transition)
+_push(void                    *data       EINA_UNUSED,
+      Estate_Cb_Type           type       EINA_UNUSED,
+      const Estate_Transition *transition EINA_UNUSED)
 {
-
+   printf("TRANSITION: <%s>\n", estate_transition_name_get(transition));
 }
 
 static void
-_push_locked(void                    *data,
-             Estate_Cb_Type           type,
+_push_locked(void                    *data       EINA_UNUSED,
+             Estate_Cb_Type           type       EINA_UNUSED,
              const Estate_Transition *transition)
 {
-
+   printf("TRANSITION: <%s>\n", estate_transition_name_get(transition));
 }
 
 static void
-_coin(void                    *data,
-      Estate_Cb_Type           type,
+_coin(void                    *data       EINA_UNUSED,
+      Estate_Cb_Type           type       EINA_UNUSED,
       const Estate_Transition *transition)
 {
-
+   printf("TRANSITION: <%s>\n", estate_transition_name_get(transition));
 }
 
 static void
-_coin_unlocked(void                    *data,
-               Estate_Cb_Type           type,
+_coin_unlocked(void                    *data       EINA_UNUSED,
+               Estate_Cb_Type           type       EINA_UNUSED,
                const Estate_Transition *transition)
 {
-
+   printf("TRANSITION: <%s>\n", estate_transition_name_get(transition));
 }
 
-#include "turnstile.est.c"
+static void
+_locked_in(void                    *data       EINA_UNUSED,
+           Estate_Cb_Type           type       EINA_UNUSED,
+           const Estate_Transition *transition EINA_UNUSED)
+{
+   printf("STATE <ENTERER> I am now locked\n");
+}
+
+static void
+_locked_out(void                    *data       EINA_UNUSED,
+            Estate_Cb_Type           type       EINA_UNUSED,
+            const Estate_Transition *transition EINA_UNUSED)
+{
+   printf("STATE <EXITER> I am not locked anymore\n");
+}
+
+static void
+_unlocked_in(void                    *data       EINA_UNUSED,
+             Estate_Cb_Type           type       EINA_UNUSED,
+             const Estate_Transition *transition EINA_UNUSED)
+{
+   printf("STATE <ENTERER> I am now unlocked\n");
+}
+
+static void
+_unlocked_out(void                    *data       EINA_UNUSED,
+              Estate_Cb_Type           type       EINA_UNUSED,
+              const Estate_Transition *transition EINA_UNUSED)
+{
+   printf("STATE <EXITER> I am not unlocked anymore\n");
+}
+
+int
+main(void)
+{
+   Estate_Machine *fsm;
+
+   fsm = estate_fsm_turnstile_load();
+   estate_machine_transition_do(fsm, "push");
+   return 0;
+}
 
