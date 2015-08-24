0e98f9e2584375edae2aa4a3f7d2becf745cb69b
Jean Guyomarc'h <jean.guyomarch@gmail.com>
2015-08-24 22:06:40 +0200

    lib: init Eina

--- src/lib/estate_main.c
+++ src/lib/estate_main.c
@@ -14,10 +14,16 @@ estate_init(void)
 {
    if (++_init_count == 1)
      {
+        if (EINA_UNLIKELY(!eina_init()))
+          {
+             EINA_LOG_CRIT("Failed to init Eina");
+             return --_init_count;
+          }
         _estate_log_dom = eina_log_domain_register("Estate", EINA_COLOR_WHITE);
         if (EINA_UNLIKELY(_estate_log_dom < 0))
           {
              EINA_LOG_CRIT("Failed to create log domain");
+             eina_shutdown();
              return --_init_count;
           }
 #if 0
@@ -35,6 +41,7 @@ estate_shutdown(void)
    if (--_init_count == 0)
      {
         eina_log_domain_unregister(_estate_log_dom);
+        eina_shutdown();
      }
    return _init_count;
 }
